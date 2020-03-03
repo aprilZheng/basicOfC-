343a07f1cd7294efdb6d98665254b22ad8fac4b2
zhengtonglin <zhengtonglin211@gmail.com>
2020-03-03 08:34:08 +0100

    * .gitignore: * error_handling.sln: * error_handling.csproj:
    * Program.cs: fist exercise, try to divide by zero

---
+++ error_handling/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace error_handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            try
+            {
+                int a = 0;
+                int b = 4;
+                int c;
+                c = b / a;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
